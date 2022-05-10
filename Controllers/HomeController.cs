b79370941f6280bc380e4989a3608c83d33be9f3
unknown <niteshlulepatil18@gmail.com>
2022-05-10 11:13:32 +0530

    Test1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication4.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Web;
+
+
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["str1"] = "This is a string passed using ViewData";
+            ViewData["num1"] = 100;
+
+            ViewBag.str2 = "This is a string passed using ViewBag";
+            ViewBag.num2 = 200;
+            return View();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        public ActionResult SaveUser(User u)
+        {
+
+            StreamWriter sw = new StreamWriter(Server.MapPath("~/App_Data/users.txt"));
+            sw.WriteLine("User details added on: " +
+             DateTime.Now.ToString());
+            sw.WriteLine("User name: " + u.UserName);
+            sw.WriteLine("Password: " + u.Password);
+            sw.WriteLine();
+            sw.Close();
+            return Content("User details have been saved");
+        }
+        public ActionResult HtmlHelpers()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication4.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
